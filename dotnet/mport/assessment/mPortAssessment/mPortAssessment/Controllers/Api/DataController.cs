cc7c2dc56bfa141b47bd5f2e222c1dfadc2bbbf0
Rui Cheng <ktei2008@gmail.com>
2014-06-16 18:01:48 +1000

    added mPortAssessment

---
+++ dotnet/mport/assessment/mPortAssessment/mPortAssessment/Controllers/Api/DataController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace mPortAssessment.Controllers.Api
+{
+    public class DataController : ApiController
+    {
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.ActionName("GetMemberSize")]
+        public HttpResponseMessage GetMemberSize([FromBody]FormDataCollection input)
+        {
+            HttpResponseMessage response = null;
+            string email = input.Get("email");
+
+            try
+            {
+                response = Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch
+            {
+                response = Request.CreateErrorResponse(HttpStatusCode.InternalServerError, new ApplicationException("Server internal error"));
+                // TODO: We should log exception
+            }
+            return response;
+        }
+    }
+}
---
+++ dotnet/mport/assessment/mPortAssessment/mPortAssessment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mPortAssessment.MvcApplication" Language="C#" %>
